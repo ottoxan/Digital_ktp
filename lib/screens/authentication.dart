b3f268032d292aa26b0dbe886f6a925e8a138356
Hilmi <massh101004@gmail.com>
2024-11-05 21:32:53 +0700

    besok lagi

---
+++ lib/screens/authentication.dart
@@ -0,0 +1,21 @@
+import 'package:local_auth/local_auth.dart';
+
+class Authentication {
+//now let's build the logic of the app
+  static final _auth = LocalAuthentication();
+
+  static Future<bool> canAuthenticate() async =>
+      await _auth.canCheckBiometrics || await _auth.isDeviceSupported();
+
+  static Future<bool> canAuthenticaion() async {
+    try {
+      if (!await canAuthenticate()) return false;
+      return await _auth.authenticate(localizedReason: "get into the app");
+    } catch (e) {
+      print('error $e');
+      return false;
+    }
+  }
+
+  static authentication() {}
+}
